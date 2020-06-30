cc39f3c09b212f59c51df60bde728c2ec386c3c7
charlie249 <56076410+charlie249@users.noreply.github.com>
2020-06-30 09:22:25 +0100

    Made a revision folder

--- GitHubUserDemo/main.js
+++ GitHubUserDemo/main.js
@@ -5,7 +5,7 @@ async function getUserData() {
     const userData = await user.json();
     document.querySelector('#avatar').setAttribute('src', userData.avatar_url);
     document.querySelector('#info').innerHTML = `<strong> Bio: </strong> ${userData.bio} <strong> Followers: </strong> ${userData.followers}`
-
+    document.querySelector('#name').innerHTML = 
 }
 
 window.addEventListener('DOMContentLoaded', () => {
