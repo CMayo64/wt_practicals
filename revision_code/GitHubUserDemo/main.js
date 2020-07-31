7ab45cb4a0edd3fc395fcc87485528f99258c43f
charlie249 <56076410+charlie249@users.noreply.github.com>
2020-07-31 11:06:13 +0100

    added everything to github

--- revision_code/GitHubUserDemo/main.js
+++ revision_code/GitHubUserDemo/main.js
@@ -4,13 +4,11 @@ async function getUserData() {
     const user = await fetch('https://api.github.com/users/CMayo64');
     const userData = await user.json();
     document.querySelector('#avatar').setAttribute('src', userData.avatar_url);
-    document.querySelector('#info').innerHTML = `<strong> Bio: </strong> ${userData.bio} <strong> Followers: </strong> ${userData.followers} <strong> Following: </strong> ${userData.following} `
-
+    document.querySelector('#info').innerHTML = `<strong> bio:</strong> ${userData.bio} <strong> following:</strong>${userData.following}`;
 }
 
 
-
-
+ 
 window.addEventListener('DOMContentLoaded', () => {
     getUserData();
 })
---
+++ revision_code/week_4_topic_2/js/main.js
@@ -0,0 +1,15 @@
+var randNum;
+var numbGuesses;
+
+function main() {
+    alert('working');
+    var randNum =  Math.round(Math.random() * (max - min) + min);
+}
+
+getRandomArbitrary(min, max) {
+    Math.round(Math.random() * (max - min) + min);
+}
+
+guess() {
+    
+}
