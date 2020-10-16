825fce1fee1ef0725ed07703105a97edaf3c2bc2
charlie249 <56076410+charlie249@users.noreply.github.com>
2020-10-16 10:44:22 +0100

    completed tasks

--- advanced_database_systems/week_2/views/common/index.js
+++ advanced_database_systems/week_2/views/common/index.js
@@ -3,9 +3,10 @@ const path = require ("path");
 
 const app = express();
 const port = 3000;
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "index.html"));
+    res.sendFile(path.resolve(__dirname, "browse.html"));
 });
 
 app.listen(port, () => {
